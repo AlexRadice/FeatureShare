6ed4e8ea1616bc9e0a4b1dcfae0edf067bbef195
Alex Radice <Alex.Radice@bjss.com>
2019-06-21 11:13:40 +0100

    Sample project with no feature files yet

---
+++ FeatureShare.Library.Tests/WidgetTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace FeatureShare.Library.Tests
+{
+	[TestFixture]
+	public class WidgetTests
+	{
+		[Test]
+		public void AddReturnsResult()
+		{
+			Assert.That(new Widget().Add(2, 3), Is.EqualTo(5));
+		}
+	}
+}
---
+++ FeatureShare.Library/Widget.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace FeatureShare.Library
+{
+	public class Widget
+	{
+		public int Add(int number1, int number2) => number1 + number2;
+	}
+}
