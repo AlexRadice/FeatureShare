d495dcf9a8c237297ca98d7a499eda296dfc0617
Alex Radice <Alex.Radice@bjss.com>
2019-06-21 11:48:42 +0100

    Sub module in place

---
+++ FeatureShare.Library.Tests/Steps/WidgetContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeatureShare.Library.Tests.Steps
+{
+	class WidgetContext
+	{
+		public Widget WidgetUnderTest { get; set; } 
+
+		public int Result { get; set; }
+	}
+}
---
+++ FeatureShare.Library.Tests/Steps/WidgetSteps.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace FeatureShare.Library.Tests.Steps
+{
+	[Binding]
+	class WidgetSteps
+	{
+		private readonly WidgetContext _widgetContext;
+
+		public WidgetSteps(WidgetContext widgetContext)
+		{
+			_widgetContext = widgetContext;
+		}
+
+		[Given(@"I have a widget")]
+		public void GivenIHaveAWidget()
+		{
+			_widgetContext.WidgetUnderTest = new Widget();
+		}
+
+		[When(@"I add (.*) to (.*)")]
+		public void WhenIAddTo(int arg1, int arg2)
+		{
+			_widgetContext.Result = _widgetContext.WidgetUnderTest.Add(arg1, arg2);
+		}
+
+		[Then(@"the result should be (.*)")]
+		public void ThenTheResultShouldBe(int expectedResult)
+		{
+			Assert.That(_widgetContext.Result, Is.EqualTo(expectedResult));
+		}
+
+	}
+}
